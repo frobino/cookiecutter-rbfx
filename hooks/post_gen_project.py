49c8e57d4e4270fc7e7e24d609b1c72126d003f6
frobino <frobino@users.noreply.github.com>
2026-01-15 21:11:34 +0100

    Add option to enable sample plugin
    Since the sample plugin is not necessarily needed,
    it is just a reference implementation of a plugin,
    a parameter to decide to have it or not in the
    output has been added.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,15 +1,18 @@
 import subprocess
 import os
 
+sample_plugin = "{{ cookiecutter.sample_plugin }}"
+
 SUBMODULE_REPO = "https://github.com/rbfx/Core.SamplePlugin.git"
 SUBMODULE_PATH = "Plugins/Core.SamplePlugin"
 
 def run(cmd):
     subprocess.check_call(cmd, shell=True)
 
-run("git init")
-if os.path.isdir(".git"):
-    run(f"git submodule add {SUBMODULE_REPO} {SUBMODULE_PATH}")
-    run("git submodule update --init --recursive")
-else:
-    print("Warning: Project is not a Git repository. Skipping submodule setup.")
+if sample_plugin == "y":
+    run("git init")
+    if os.path.isdir(".git"):
+        run(f"git submodule add {SUBMODULE_REPO} {SUBMODULE_PATH}")
+        run("git submodule update --init --recursive")
+    else:
+        print("Warning: Project is not a Git repository. Skipping submodule setup.")
